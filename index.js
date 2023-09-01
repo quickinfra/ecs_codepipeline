c0063fa63eb34c519fb518f5099a34997ee52527
feroz <feroz@quickinfracloud.com>
2023-09-01 19:53:44 +0530

    Updated to Version-2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Welcome to my app : Version-1");
+  res.send("Welcome to my app : Version-2");
 });
 
 app.get("/info", (req, res) => {
